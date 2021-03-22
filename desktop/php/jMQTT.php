31e84eef3c9ff4194f8777c70869daabbb584eb7
Nebz <28622481+NebzHB@users.noreply.github.com>
2021-03-22 08:13:17 +0100

    don't use the minified

--- desktop/php/jMQTT.php
+++ desktop/php/jMQTT.php
@@ -265,7 +265,7 @@ function displayEqLogicCard($eqL, $node_images) {
     </div>
 </div>
 
-<?php include_file('desktop', 'jMQTT-min', 'js', 'jMQTT'); ?>
+<?php include_file('desktop', 'jMQTT', 'js', 'jMQTT'); ?>
 <?php include_file('core', 'plugin.template', 'js'); ?>
 <?php include_file('3rdparty', 'jquery.treegrid', 'css', 'jMQTT'); ?>
 <?php include_file('3rdparty', 'jquery.treegrid.min', 'js', 'jMQTT'); ?>
