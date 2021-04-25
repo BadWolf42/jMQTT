93250077f66a76cbe8dfbe4c8878b4bce646f0e5
DomoChip <domochip@hotmail.com>
2021-04-25 22:05:18 +0200

    Add jMQTTBase

---
+++ core/class/jMQTTBase.class.php
@@ -0,0 +1,187 @@
+<?php
+/* * ***************************Includes********************************* */
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+
+include_file('3rdparty', 'mosquitto_topic_matches_sub', 'php', 'jMQTT');
+
+class jMQTTBase extends eqLogic {
+
+   const DEFAULT_PYTHON_PORT = 55666;
+   const DEFAULT_WEBSOCKET_PORT = 55667;
+
+   public static function dependancy_info() {
+      return jMQTT::dependancy_info();
+   }
+
+   public static function dependancy_install() {
+      return array();
+   }
+
+
+   public static function deamon_info() {
+      $return = array();
+      $return['log'] = get_called_class();
+      $return['state'] = 'nok';
+      $return['launchable'] = 'nok';
+
+      $python_daemon = false;
+      $websocket_daemon = false;
+
+      $pid_file1 = jeedom::getTmpFolder(get_called_class()) . '/jmqttd.py.pid';
+      if (file_exists($pid_file1)) {
+         if (@posix_getsid(trim(file_get_contents($pid_file1)))) {
+            $python_daemon = true;
+         } else {
+            shell_exec(system::getCmdSudo() . 'rm -rf ' . $pid_file1 . ' 2>&1 > /dev/null');
+         }
+      }
+
+      $pid_file2 = jeedom::getTmpFolder(get_called_class()) . '/jmqttd.php.pid';
+      if (file_exists($pid_file2)) {
+         if (@posix_getsid(trim(file_get_contents($pid_file2)))) {
+            $websocket_daemon = true;
+         } else {
+            shell_exec(system::getCmdSudo() . 'rm -rf ' . $pid_file2 . ' 2>&1 > /dev/null');
+         }
+      }
+
+      if($python_daemon && $websocket_daemon){
+         $return['state'] = 'ok';
+      }
+
+      if (config::byKey('pythonsocketport', get_called_class(), get_called_class()::DEFAULT_PYTHON_PORT) != config::byKey('websocketport', get_called_class(), get_called_class()::DEFAULT_WEBSOCKET_PORT)) {
+         $return['launchable'] = 'ok';
+      }
+      return $return;
+   }
+
+   public static function deamon_start() {
+      self::deamon_stop();
+      $daemon_info = self::deamon_info();
+      if ($daemon_info['launchable'] != 'ok') {
+         throw new Exception(__('Veuillez vérifier la configuration', __FILE__));
+      }
+
+      $path1 = realpath(dirname(__FILE__) . '/../../resources/jmqttd');
+      $cmd1 = 'python3 ' . $path1 . '/jmqttd.py';
+      $cmd1 .= ' --plugin ' . get_called_class();
+      $cmd1 .= ' --loglevel ' . log::convertLogLevel(log::getLogLevel(get_called_class()));
+      $cmd1 .= ' --socketport ' . config::byKey('pythonsocketport', get_called_class(), get_called_class()::DEFAULT_PYTHON_PORT);
+      $cmd1 .= ' --apikey ' . jeedom::getApiKey(get_called_class());
+      $cmd1 .= ' --pid ' . jeedom::getTmpFolder(get_called_class()) . '/jmqttd.py.pid';
+      log::add(get_called_class(), 'info', 'Lancement du démon python jMQTT pour le plugin '.get_called_class());
+      $result1 = exec($cmd1 . ' >> ' . log::getPathToLog(get_called_class().'_daemon') . ' 2>&1 &');
+
+      $path2 = realpath(dirname(__FILE__) . '/../../core/php/');
+      $cmd2 = 'php ' . $path2 . '/jmqttd.php';
+      $cmd2 .= ' --plugin ' . get_called_class();
+      $cmd2 .= ' --socketport ' . config::byKey('websocketport', get_called_class(), get_called_class()::DEFAULT_WEBSOCKET_PORT);
+      $cmd2 .= ' --pid ' . jeedom::getTmpFolder(get_called_class()) . '/jmqttd.php.pid';
+      log::add(get_called_class(), 'info', 'Lancement du démon websocket jMQTT pour le plugin '.get_called_class());
+      $result2 = exec($cmd2 . ' >> ' . log::getPathToLog(get_called_class()) . ' 2>&1 &');
+
+      $i = 0;
+      while ($i < 10) {
+         $daemon_info = self::deamon_info();
+         if ($daemon_info['state'] == 'ok') {
+            break;
+         }
+         sleep(1);
+         $i++;
+      }
+      if ($i >= 10) {
+         log::add(get_called_class(), 'error', __('Impossible de lancer le démon jMQTT pour le plugin '.get_called_class().', vérifiez le log',__FILE__), 'unableStartDaemon');
+         return false;
+      }
+      message::removeAll(get_called_class(), 'unableStartDaemon');
+      return true;
+   }
+
+   public static function deamon_stop() {
+      $pid_file1 = jeedom::getTmpFolder(get_called_class()) . '/jmqttd.py.pid';
+      if (file_exists($pid_file1)) {
+         $pid1 = intval(trim(file_get_contents($pid_file1)));
+         system::kill($pid1, false);
+      }
+      $pid_file2 = jeedom::getTmpFolder(get_called_class()) . '/jmqttd.php.pid';
+      if (file_exists($pid_file2)) {
+         $pid2 = intval(trim(file_get_contents($pid_file2)));
+         system::kill($pid2, false);
+      }
+      system::fuserk(config::byKey('pythonsocketport', get_called_class(), get_called_class()::DEFAULT_PYTHON_PORT));
+      system::fuserk(config::byKey('websocketport', get_called_class(), get_called_class()::DEFAULT_WEBSOCKET_PORT));
+      sleep(1);
+   }
+
+   public static function on_daemon_connect() {
+      log::add(get_called_class(), 'debug', 'You need to implement "public static function on_daemon_connect()" in the class \''.get_called_class().'\' to handle daemon connect event.');
+   }
+   public static function on_daemon_disconnect() {
+      log::add(get_called_class(), 'debug', 'You need to implement "public static function on_daemon_disconnect()" in the class \''.get_called_class().'\' to handle daemon disconnect event.');
+   }
+   public static function on_mqtt_connect() {
+      log::add(get_called_class(), 'debug', 'You need to implement "public static function on_mqtt_connect()" in the class \''.get_called_class().'\' to handle mqtt connect event.');
+   }
+   public static function on_mqtt_disconnect() {
+      log::add(get_called_class(), 'debug', 'You need to implement "public static function on_mqtt_disconnect()" in the class \''.get_called_class().'\' to handle mqtt disconnect event.');
+   }
+   public static function on_mqtt_message() {
+      log::add(get_called_class(), 'debug', 'You need to implement "public static function on_mqtt_message()" in the class \''.get_called_class().'\' to handle mqtt messages.');
+   }
+   
+
+   protected static function send_to_mqtt_daemon($params) {
+      $daemon_info = self::deamon_info();
+      if ($daemon_info['state'] != 'ok') {
+         throw new Exception("Le démon n'est pas démarré");
+      }
+      $params['apikey'] = jeedom::getApiKey(get_called_class());
+      $payload = json_encode($params);
+      $socket = socket_create(AF_INET, SOCK_STREAM, 0);
+      socket_connect($socket, '127.0.0.1', config::byKey('pythonsocketport', get_called_class(), get_called_class()::DEFAULT_PYTHON_PORT));
+      socket_write($socket, $payload, strlen($payload));
+      socket_close($socket);
+   }
+
+   public static function new_mqtt_client($id, $hostname, $port, $clientid = '', $statustopic = '') {
+      $params['cmd']='newMqttClient';
+      $params['id']=$id;
+      $params['callback']='ws://127.0.0.1:'.config::byKey('websocketport', get_called_class(), get_called_class()::DEFAULT_WEBSOCKET_PORT).'/plugins/template/core/php/jmqttd.php';
+      $params['hostname']=$hostname;
+      $params['port']=$port;
+      $params['clientid']=$clientid;
+      $params['statustopic']=$statustopic;
+      get_called_class()::send_to_mqtt_daemon($params);
+   }
+
+   public static function remove_mqtt_client($id) {
+      $params['cmd']='removeMqttClient';
+      $params['id']=$id;
+      get_called_class()::send_to_mqtt_daemon($params);
+   }
+
+   public static function subscribe_mqtt_topic($id, $topic, $qos = 1) {
+      $params['cmd']='subscribeTopic';
+      $params['id']=$id;
+      $params['topic']=$topic;
+      $params['qos']=$qos;
+      get_called_class()::send_to_mqtt_daemon($params);
+   }
+
+   public static function unsubscribe_mqtt_topic($id, $topic) {
+      $params['cmd']='unsubscribeTopic';
+      $params['id']=$id;
+      $params['topic']=$topic;
+      get_called_class()::send_to_mqtt_daemon($params);
+   }
+
+   public static function send_mqtt_message($id, $topic, $payload, $qos = 1, $retain = false) {
+      $params['cmd']='messageOut';
+      $params['id']=$id;
+      $params['topic']=$topic;
+      $params['payload']=$payload;
+      $params['qos']=$qos;
+      $params['retain']=$retain;
+      get_called_class()::send_to_mqtt_daemon($params);
+   }
+}
