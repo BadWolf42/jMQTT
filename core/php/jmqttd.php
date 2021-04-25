86ff153830d2f11daa265a2548f626441880f4a5
DomoChip <domochip@hotmail.com>
2021-04-25 21:47:56 +0200

    add new daemon files

---
+++ core/php/jmqttd.php
@@ -0,0 +1,143 @@
+<?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+require_once dirname(__FILE__) . '/../../3rdparty/vendor/autoload.php';
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+$options = getopt('',array('plugin:','socketport:','pid:'));
+if (array_key_exists('plugin', $options)) {
+    $plugin = $options['plugin'];
+}
+else {
+    echo("[ERROR] !!! plugin name / class not provided !!! => Exit\n");
+    return;
+}
+if (array_key_exists('pid', $options)) {
+    $pidfile = $options['pid'];
+}
+else {
+    log::add($plugin, 'error', 'pidfile path not provided => Exit');
+    return;
+}
+if (array_key_exists('socketport', $options)) {
+    $socketport = intval($options['socketport']);
+}
+else {
+    log::add($plugin, 'error', 'socketport not provided => Exit');
+    return;
+}
+
+log::add($plugin, 'info', 'Start jMQTT websocket daemon');
+log::add($plugin, 'info', 'Plugin : ' . $plugin);
+log::add($plugin, 'info', 'Socket port : ' . $socketport);
+log::add($plugin, 'info', 'PID file : ' . $pidfile);
+
+// Create/overwrite pid file
+$pid = posix_getpid();
+log::add($plugin, 'debug', 'Writing PID ' . $pid . ' to ' . $pidfile);
+$fp = fopen($pidfile, 'w+');
+if($fp == false){
+    log::add($plugin, 'error', 'Can\'t open pidfile => Exit');
+    return;
+}
+fwrite($fp, $pid);
+fclose($fp);
+
+
+class jMQTTdLogic implements MessageComponentInterface {
+
+    private $plugin;
+
+    public function __construct(string $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+
+        if ( ! $conn->httpRequest->hasHeader('id') || ! $conn->httpRequest->hasHeader('apikey')) {
+            $conn->send('Vous n\'etes pas autorisé à effectuer cette action', __FILE__);
+            $conn->close();
+        }
+
+        if (!jeedom::apiAccess($conn->httpRequest->getHeader('apikey')[0], $this->plugin)) {
+            $conn->send('Vous n\'etes pas autorisé à effectuer cette action', __FILE__);
+            $conn->close();
+        }
+
+        log::add($this->plugin, 'debug', sprintf('Id %d : Python daemon connected successfully to WebSocket Daemon', $conn->httpRequest->getHeader('id')[0]));
+
+        if(method_exists($this->plugin, 'on_daemon_connect')) {
+            $this->plugin::on_daemon_connect($conn->httpRequest->getHeader('id')[0]);
+        }
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+
+        log::add($this->plugin, 'debug', sprintf('Id %d : onMessage received : "%s"', $from->httpRequest->getHeader('id')[0], $msg));
+
+        $message = json_decode($msg, true);
+        
+        if ($message == null) {
+            log::add($this->plugin, 'error', sprintf('Received message is not a correct JSON!? : %s', $msg));
+            return;
+        }
+        if (!array_key_exists('cmd', $message)) {
+            log::add($this->plugin, 'error', sprintf('Received message doesn\'t contain cmd!? : %s', $msg));
+            return;
+        }
+
+        switch ($message['cmd']) {
+            case 'connection':
+                if ($message['state']) {
+                    if(method_exists($this->plugin, 'on_mqtt_connect')) {
+                        $this->plugin::on_mqtt_connect($from->httpRequest->getHeader('id')[0]);
+                    }
+                }
+                else {
+                    if(method_exists($this->plugin, 'on_mqtt_disconnect')) {
+                        $this->plugin::on_mqtt_disconnect($from->httpRequest->getHeader('id')[0]);
+                    }
+                }
+                break;
+            case 'messageIn':
+                if(method_exists($this->plugin, 'on_mqtt_message')) {
+                    $this->plugin::on_mqtt_message($from->httpRequest->getHeader('id')[0], $message['topic'], $message['payload'], $message['qos'], $message['retain']);
+                }
+                break;
+            default:
+                log::add($this->plugin, 'error', sprintf('Id %d : Received message contains unkown cmd!?', $from->httpRequest->getHeader('id')[0]));
+                break;
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        log::add($this->plugin, 'debug', sprintf('Id %d : Python daemon disconnected from WebSocket Daemon', $conn->httpRequest->getHeader('id')[0]));
+
+        if(method_exists($this->plugin, 'on_daemon_disconnect')) {
+            $this->plugin::on_daemon_disconnect($conn->httpRequest->getHeader('id')[0]);
+        }
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        log::add($this->plugin, 'error', sprintf('Id %d : Unexpected error between WebSocket Daemon and Python Daemon', $conn->httpRequest->getHeader('id')[0], $e->getMessage()));
+
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new jMQTTdLogic($plugin)
+        )
+    ),
+    $socketport
+);
+
+
+log::add($plugin, 'debug', 'Listening on: [127.0.0.1:' . $socketport . ']');
+$server->run();
---
+++ resources/jmqttd/jmqttd.py
@@ -0,0 +1,434 @@
+# This file is part of Jeedom.
+#
+# Jeedom is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Jeedom is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import string
+import sys
+import os
+import time
+import datetime
+import argparse
+import re
+import signal
+from optparse import OptionParser
+from os.path import join
+import json
+import websocket
+import paho.mqtt.client as mqtt
+from threading import Thread
+from queue import Queue
+import gc
+
+try:
+	from jeedom.jeedom import jeedom_socket
+	from jeedom.jeedom import jeedom_utils
+	from jeedom.jeedom import JEEDOM_SOCKET_MESSAGE
+except ImportError:
+	print("Error: importing module jeedom.jeedom")
+	sys.exit(1)
+
+class MqttClient:
+	def __init__(self, queue, message):
+		self.q = queue
+		self.id = message['id']
+
+		self.mqttstatustopic = message['statustopic']
+		self.mqtthostname = message['hostname']
+		self.mqttport = message['port']
+		self.mqttsubscribedtopics = {}
+
+		self.connected = False
+
+		# Create MQTT Client
+		self.mqttclient = mqtt.Client()
+		if 'username' in message and message['username'] != '':
+			if 'password' in message and message['password'] != '':
+				self.mqttclient.username_pw_set(message['username'], message['password'])
+			else:
+				self.mqttclient.username_pw_set(message['username'])
+		self.mqttclient.reconnect_delay_set(5, 15)
+		self.mqttclient.on_connect = self.on_connect
+		self.mqttclient.on_disconnect = self.on_disconnect
+		self.mqttclient.on_message = self.on_message
+		self.mqttthread = Thread(target=self.mqttclient.loop_forever)
+
+		self.start()
+
+	def on_connect(self, client, userdata, flags, rc):
+		self.connected = True
+		if self.mqttstatustopic != '':
+			client.will_set(self.mqttstatustopic, 'offline', 1, True)
+			client.publish(self.mqttstatustopic, 'online', 1, True)
+		logging.info('Id ' + str(self.id) + ' : Connected to broker ' + self.mqtthostname + ':' + str(self.mqttport))
+		for topic in self.mqttsubscribedtopics:
+			self.subscribe_topic(topic, self.mqttsubscribedtopics[topic])
+		self.q.put('{"cmd":"connection","state":' + self.is_connected() + '}')
+
+	def on_disconnect(self, client, userdata, rc):
+		self.connected = False
+		self.q.put('{"cmd":"connection","state":' + self.is_connected() + '}')
+		if rc == mqtt.MQTT_ERR_SUCCESS:
+			logging.info('Id ' + str(self.id) + ' : Disconnected from broker.')
+		else:
+			logging.error('Id ' + str(self.id) + ' : Unexpected disconnection from broker!')
+
+	def on_message(self, client, userdata, message):
+		logging.info('Id ' + str(self.id) + ' : message received (topic="' + message.topic + '", payload="' + message.payload.decode('utf-8') + '", QoS='+ str(message.qos) + ', retain=' + str(message.retain) + ')')
+		self.q.put('{"cmd":"messageIn","topic":"'+ message.topic +'","payload":"' + message.payload.decode('utf-8') + '","qos":' + str(message.qos) + ',"retain":' + str(message.retain) + '}')
+
+	def is_connected(self):
+		return str(self.connected).lower()
+
+	def subscribe_topic(self, topic, qos):
+		res = self.mqttclient.subscribe(topic, qos)
+		if res[0] == mqtt.MQTT_ERR_SUCCESS or res[0] == mqtt.MQTT_ERR_NO_CONN:
+			self.mqttsubscribedtopics[topic] = qos
+			logging.info('Id ' + str(self.id) + ' : Topic subscribed "' + topic + '"')
+		else:
+			logging.error('Id ' + str(self.id) + ' : Topic subscription failed "' + topic + '"')
+
+	def unsubscribe_topic(self, topic):
+		if topic in self.mqttsubscribedtopics:
+			res = self.mqttclient.unsubscribe(topic)
+			if res[0] == mqtt.MQTT_ERR_SUCCESS or res[0] == mqtt.MQTT_ERR_NO_CONN:
+				del self.mqttsubscribedtopics[topic]
+				logging.info('Id ' + str(self.id) + ' : Topic unsubscribed "' + topic + '"')
+			else:
+				logging.error('Id ' + str(self.id) + ' : Topic unsubscription failed "' + topic + '"')
+		else:
+			logging.info('Id ' + str(self.id) + ' : Can\'t unsubscribe not subscribed topic "' + topic)
+
+	def publish(self, topic, payload, qos, retain):
+		self.mqttclient.publish(topic, payload, qos, retain)
+		logging.info('Id ' + str(self.id) + ' : Sending message to broker (topic="' + topic + '", payload="' + payload + '", QoS='+ str(qos) + ', retain=' + str(retain) + ')')
+
+	def start(self):
+		try:
+			self.mqttclient.connect(self.mqtthostname, self.mqttport, 30)
+		except:
+			pass
+		self.mqttthread.start()
+
+	def stop(self):
+		self.mqttclient.publish(self.mqttstatustopic, 'offline', 1, True).wait_for_publish()
+		self.mqttclient.disconnect()
+		self.mqttthread.join()
+	
+class WebSocketClient:
+	def __init__(self, queue, message, fnismqttconnected):
+		self.q = queue
+		self.id = message['id']
+		self.apikey = message['apikey']
+		self.wscallback = message['callback']
+		self.fnismqttconnected = fnismqttconnected
+		
+		# Create WebSocket Client
+		self.wsclient = websocket.WebSocketApp(
+			url=self.wscallback,
+			header={"apikey" : self.apikey, "id" : str(self.id)},
+			on_open=self.on_open,
+			on_message=self.on_message,
+			on_close=self.on_close,
+			on_error=self.on_error)
+
+		self.wsthread = Thread(target=self.autorestart_run_forever)
+		self.workerthread = Thread(target=self.worker)
+
+		self.start()
+
+	def autorestart_run_forever(self):
+		while True:
+			try:
+				self.wsclient.run_forever(skip_utf8_validation=True, ping_interval=150, ping_timeout=1)
+			except:
+				pass
+			if self.stopautorestart:
+				break
+			time.sleep(5)
+
+	def worker(self):
+		while True:
+			try:
+				msg = self.q.get(block=True, timeout=0.1)
+				self.wsclient.send(msg)
+				logging.info('Id ' + str(self.id) + ' : Sending message to Jeedom ' + msg)
+			except:
+				pass
+			if self.stopworker and self.q.empty():
+				break
+
+	def on_open(self, ws):
+		ws.send('{"cmd":"connection","state":' + str(self.fnismqttconnected()) + '}')
+		logging.info('Id ' + str(self.id) + ' : Connected to Jeedom using ' + self.wscallback)
+
+	def on_message(self, ws, message):
+		logging.debug('Id ' + str(self.id) + ' : Received a message through WebSocket !!!')
+
+	def on_error(self, ws, error):
+		if not isinstance(error, AttributeError) or not self.stopautorestart:
+			logging.error('Id ' + str(self.id) + ' : WebSocket client encountered an Error!')
+
+	def on_close(self, ws):
+		logging.info('Id ' + str(self.id) + ' : Disconnected from Jeedom')
+
+	def start(self):
+		self.stopautorestart = False
+		self.wsthread.start()
+		self.stopworker = False
+		self.workerthread.start()
+
+	def stop(self):
+		self.stopworker = True
+		self.workerthread.join()
+		self.stopautorestart = True
+		self.wsclient.close()
+		self.wsthread.join()
+
+
+class jMqttClient:
+	def __init__(self, message):
+		self.q = Queue()
+		self.mqtt = MqttClient(self.q, message)
+		self.ws = WebSocketClient(self.q, message, self.mqtt.is_connected)
+
+	def start(self):
+		self.mqtt.start()
+		self.ws.start()
+
+	def stop(self):
+		self.mqtt.stop()
+		self.ws.stop()
+
+
+def cmd_handler(message):
+	# Make some controls on received message
+	if 'id' not in message:
+		logging.error('!!! id is missing !!! : ' + json.dumps(message))
+		return
+	if 'cmd' not in message:
+		logging.error('!!! cmd is missing !!! : ' + json.dumps(message))
+		return
+
+	
+	# Make some automatic convertions on received message
+	if type(message['id']) is str:
+		try:
+			message['id'] = int(message['id'])
+		except:
+			logging.error('!!! Incorrect id provided : ' + message['id'])
+			return
+	if 'port' in message and type(message['port']) is str:
+		try:
+			message['port'] = int(message['port'])
+		except:
+			logging.error('Id ' + str(message['id']) + ' !!! Incorrect port provided : ' + message['port'])
+			return
+	if 'qos' in message and type(message['qos']) is str:
+		try:
+			message['qos'] = int(message['qos'])
+		except:
+			logging.error('Id ' + str(message['id']) + ' !!! Incorrect qos provided : ' + message['qos'])
+			return
+	if 'retain' in message and type(message['retain']) is str:
+		try:
+			message['retain'] = bool(message['retain'])
+		except:
+			logging.error('Id ' + str(message['id']) + ' !!! Incorrect retain provided : ' + message['retain'])
+			return
+
+	# ------------------------------ newMqttClient ------------------------------
+	if message['cmd'] == 'newMqttClient':
+		#Make more controls on received message
+		if not (message.keys() >= {'callback', 'hostname', 'port', 'clientid', 'statustopic'}):
+			logging.error('Id ' + str(message['id']) + ' !!! newMqttClient - missing parameter : ' + json.dumps(message))
+			return
+
+		# if jmqttclient already exists then remove it first
+		if message['id'] in jmqttclients:
+			logging.info('Id ' + str(message['id']) + ' : Client already exists. Starting removal')
+			jmqttclients[message['id']].stop()
+			del jmqttclients[message['id']]
+
+		# create requested jmqttclient
+		logging.info('Id ' + str(message['id']) + ' : Starting Client creation')
+		newjMqttClient = jMqttClient(message)
+		jmqttclients[message['id']] = newjMqttClient
+		
+
+	# ------------------------------ removeMqttClient ------------------------------
+	elif message['cmd'] == 'removeMqttClient':
+
+		# if jmqttclient exists then remove it
+		if message['id'] in jmqttclients:
+			logging.info('Id ' + str(message['id']) + ' : Starting Client removal')
+			jmqttclients[message['id']].stop()
+			del jmqttclients[message['id']]
+		else:
+			logging.info('Id ' + str(message['id']) + ' : No client found with this Id')
+			
+
+	# ------------------------------ subscribeTopic ------------------------------
+	elif message['cmd'] == 'subscribeTopic':
+		#Make more controls on received message
+		if not (message.keys() >= {'topic', 'qos'}):
+			logging.error('Id ' + str(message['id']) + ' !!! subscribeTopic - missing parameter : ' + json.dumps(message))
+			return
+		if message['topic'] == '':
+			logging.error('Id ' + str(message['id']) + ' !!! subscribeTopic - topic cannot be empty : ' + json.dumps(message))
+			return
+
+		if message['id'] in jmqttclients:
+			jmqttclients[message['id']].mqtt.subscribe_topic(message['topic'], message['qos'])
+
+	# ------------------------------ unsubscribeTopic ------------------------------
+	elif message['cmd'] == 'unsubscribeTopic':
+		#Make more controls on received message
+		if not (message.keys() >= {'topic'}):
+			logging.error('Id ' + str(message['id']) + ' !!! unsubscribeTopic - missing parameter : ' + json.dumps(message))
+			return
+		if message['topic'] == '':
+			logging.error('Id ' + str(message['id']) + ' !!! unsubscribeTopic - topic cannot be empty : ' + json.dumps(message))
+			return
+
+		if message['id'] in jmqttclients:
+			jmqttclients[message['id']].mqtt.unsubscribe_topic(message['topic'])
+
+	# ------------------------------ messageOut ------------------------------
+	elif message['cmd'] == 'messageOut':
+		if not (message.keys() >= {'topic','payload','qos','retain'}):
+			logging.error('Id ' + str(message['id']) + ' !!! messageOut - missing parameter : ' + json.dumps(message))
+			return
+		if message['topic'] == '':
+			logging.error('Id ' + str(message['id']) + ' !!! messageOut - topic cannot be empty : ' + json.dumps(message))
+			return
+		if message['qos'] < 0 or message['qos'] > 2:
+			logging.error('Id ' + str(message['id']) + ' !!! messageOut - qos wrong value : ' + json.dumps(message))
+			return
+
+		if message['id'] in jmqttclients:
+			jmqttclients[message['id']].mqtt.publish(message['topic'], message['payload'], message['qos'], message['retain'])
+
+	else:
+		logging.error('Unknown cmd : ' + json.dumps(message))
+
+
+def listen():
+	global JEEDOM_SOCKET_MESSAGE
+	jeedomsocket.open()
+	while True:
+		try:
+			jeedom_msg = JEEDOM_SOCKET_MESSAGE.get(block=True, timeout=0.1).decode('utf-8')
+		except KeyboardInterrupt:
+			shutdown()
+		except:
+			pass
+		else:
+			logging.debug('jeedom_socket received message : ' + jeedom_msg)
+			try:
+				message = json.loads(jeedom_msg)
+			except:
+				logging.debug('jeedom_socket received message is not a correct JSON')
+			else:
+				if message['apikey'] != _apikey:
+					logging.error("Invalid apikey from socket : " + str(message))
+					return
+				cmd_handler(message)
+
+
+# ----------------------------------------------------------------------------
+
+def signal_handler(signum=None, frame=None):
+	logging.debug("Signal %i caught, exiting..." % int(signum))
+	shutdown()
+
+def shutdown():
+	logging.debug("Shutdown")
+	logging.debug("Removing PID file " + str(_pidfile))
+	try:
+		os.remove(_pidfile)
+	except:
+		pass
+	try:
+		jeedomsocket.close()
+	except:
+		pass
+	# try:
+	# 	jeedom_serial.close()
+	# except:
+	# 	pass
+	try:
+		for id in list(jmqttclients):
+			jmqttclients[id].stop()
+			del jmqttclients[id]
+	except:
+		pass
+	logging.debug("Exit 0")
+	sys.stdout.flush()
+	os._exit(0)
+
+# ----------------------------------------------------------------------------
+
+_plugin = ''
+_log_level = "error"
+_socket_port = 55666
+_socket_host = '127.0.0.1'
+_pidfile = '/tmp/jmqttd.pid'
+_apikey = ''
+
+
+parser = argparse.ArgumentParser(description='Daemon for Jeedom plugin')
+parser.add_argument("--plugin", help="Name of the plugin", type=str)
+parser.add_argument("--loglevel", help="Log Level for the daemon", type=str)
+parser.add_argument("--socketport", help="Socketport for server", type=int)
+parser.add_argument("--apikey", help="Apikey", type=str)
+parser.add_argument("--pid", help="Pid file", type=str)
+args = parser.parse_args()
+
+#  Plugin name is mandatory
+if args.plugin:
+	_plugin = args.plugin
+else:
+	shutdown()
+if args.loglevel:
+	_log_level = args.loglevel
+if args.socketport:
+	_socket_port = args.socketport
+if args.apikey:
+	_apikey = args.apikey
+if args.pid:
+	_pidfile = args.pid
+
+
+jeedom_utils.set_log_level(_log_level)
+
+logging.info('Start jMQTT python daemon')
+logging.info('Plugin : '+str(_plugin))
+logging.info('Log level : '+str(_log_level))
+logging.info('Socket port : '+str(_socket_port))
+logging.info('PID file : '+str(_pidfile))
+logging.debug('Apikey : '+str(_apikey))
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+jmqttclients = {}
+
+try:
+	jeedom_utils.write_pid(str(_pidfile))
+	jeedomsocket = jeedom_socket(port=_socket_port,address=_socket_host)
+	listen()
+except Exception as e:
+	logging.error('Fatal error : '+str(e))
+	shutdown()
