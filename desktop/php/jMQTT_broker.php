d7d7a6707feff87b827b272ae50201d9c72cf61c
DomoChip <domochip@hotmail.com>
2021-04-29 00:25:54 +0200

    daemon title in broker eqpt page to MQTT Client

--- desktop/php/jMQTT_broker.php
+++ desktop/php/jMQTT_broker.php
@@ -4,7 +4,7 @@
         <div class="panel panel-success">
             <div class="panel-heading">
                 <h3 class="panel-title">
-                    <i class="fa fa-university"></i> {{Démon}}
+                    <i class="fa fa-university"></i> {{Client MQTT}}
                 </h3>
             </div>
             <div class="panel-body">
