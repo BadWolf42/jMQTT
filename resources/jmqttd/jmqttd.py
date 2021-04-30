4152c197d745c73ef6f7da11f028e88c9143b8ad
DomoChip <domochip@hotmail.com>
2021-04-30 12:53:16 +0200

    Add unhandled exception text

--- resources/jmqttd/jmqttd.py
+++ resources/jmqttd/jmqttd.py
@@ -460,5 +460,5 @@ try:
 	jeedomsocket = jeedom_socket(port=_socket_port,address=_socket_host)
 	listen()
 except Exception as e:
-	logging.exception('Fatal unhandled Exception')
+	logging.exception('Fatal unhandled Exception : %s', e)
 	shutdown()
