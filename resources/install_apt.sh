2a691b47dcce781e0194f8ad25ed56cbdf15246a
DomoChip <domochip@hotmail.com>
2022-02-17 18:28:11 +0100

    Update install_apt.sh for splitted composer

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -33,20 +33,26 @@ if [ ${INSTALL_MOSQUITTO} -eq 1 ]; then
 	fi
 fi
 
-step 40 "Install Ratchet PHP library"
-
+step 20 "Install Composer"
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ../resources
 try php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 try php composer-setup.php
 try php -r "unlink('composer-setup.php');"
-try sudo -u www-data php ./composer.phar update --working-dir=.
-silent rm composer.phar
 
-step 45 "Temporary fix of Ratchet PHP library (https://github.com/ratchetphp/RFC6455/pull/65)"
+step 25 "Install Ratchet PHP library"
+try sudo -u www-data php ./composer.phar update --working-dir=./Ratchet
+
+step 30 "Temporary fix of Ratchet PHP library (https://github.com/ratchetphp/RFC6455/pull/65)"
 if [ $(getconf LONG_BIT) -eq 32 ]; then
-	try sed -i -E "s/unpack\('J'(.*)\)\[1\]/unpack('N2'\1)[2]/g" ./vendor/ratchet/rfc6455/src/Messaging/MessageBuffer.php
+	try sed -i -E "s/unpack\('J'(.*)\)\[1\]/unpack('N2'\1)[2]/g" ./Ratchet/vendor/ratchet/rfc6455/src/Messaging/MessageBuffer.php
 fi
+
+step 35 "Install JsonPath-PHP library"
+try sudo -u www-data php ./composer.phar update --working-dir=./JsonPath-PHP
+
+step 40 "Remove Composer"
+silent rm composer.phar
 
 step 50 "Install python3 debian packages"
 try sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-requests python3-pip python3-wheel
