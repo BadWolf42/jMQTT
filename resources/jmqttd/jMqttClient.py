372591f0a3b712f39efb44f8858d76845b1ad3c6
Bad <BadWolf42@users.noreply.github.com>
2022-06-30 09:40:15 +0200

    Fix logging in Python class jMqttClient

--- resources/jmqttd/jMqttClient.py
+++ resources/jmqttd/jMqttClient.py
@@ -31,9 +31,10 @@ except ImportError:
 
 class jMqttClient:
 	def __init__(self, jcom, message):
-#		logging.debug('MqttClient.init(): message=%r', message)
-		self.jcom = jcom
-		self.message = message
+		self._log       = logging.getLogger('jMqttClient')
+#		self._log.debug('MqttClient.init(): message=%r', message)
+		self.jcom       = jcom
+		self.message    = message
 		self.mqttclient = None
 
 	def on_connect(self, client, userdata, flags, rc):
@@ -41,7 +42,7 @@ class jMqttClient:
 		if self.mqttstatustopic != '':
 			client.will_set(self.mqttstatustopic, 'offline', 1, True)
 			client.publish(self.mqttstatustopic, 'online', 1, True)
-		logging.info('BrkId: % 4s : Connected to broker %s:%d', self.id, self.mqtthostname, self.mqttport)
+		self._log.info('BrkId: % 4s : Connected to broker %s:%d', self.id, self.mqtthostname, self.mqttport)
 		with self.mqttsub_lock:
 			for topic in self.mqttsubscribedtopics:
 				self.subscribe_topic(topic, self.mqttsubscribedtopics[topic], False)
@@ -51,9 +52,9 @@ class jMqttClient:
 		self.connected = False
 		self.jcom.send_async({"cmd":"brokerDown","id":self.id})
 		if rc == mqtt.MQTT_ERR_SUCCESS:
-			logging.info('BrkId: % 4s : Disconnected from broker.', self.id)
+			self._log.info('BrkId: % 4s : Disconnected from broker.', self.id)
 		else:
-			logging.error('BrkId: % 4s : Unexpected disconnection from broker!', self.id)
+			self._log.error('BrkId: % 4s : Unexpected disconnection from broker!', self.id)
 
 	def on_message(self, client, userdata, message):
 		try:
@@ -67,7 +68,7 @@ class jMqttClient:
 			except: # If payload cannot be decoded or decompressed it is returned in base64
 				usablePayload = b2a_base64(message.payload, newline=False).decode('utf-8')
 				form = ' (bin in base64)'
-			logging.info('BrkId: % 4s : Message received (topic="%s", payload="%s"%s, QoS=%s, retain=%s)', self.id, message.topic, usablePayload, form, message.qos, bool(message.retain))
+		self._log.info('BrkId: % 4s : Message received (topic="%s", payload="%s"%s, QoS=%s, retain=%s)', self.id, message.topic, usablePayload, form, message.qos, bool(message.retain))
 		self.jcom.send_async({"cmd":"messageIn","id":self.id,"topic":message.topic,"payload":usablePayload,"qos":message.qos,"retain":bool(message.retain)})
 
 	def subscribe_topic(self, topic, qos, lock=True):
@@ -77,30 +78,30 @@ class jMqttClient:
 				if lock:
 					with self.mqttsub_lock:
 						self.mqttsubscribedtopics[topic] = qos
-				logging.info('BrkId: % 4s : Topic subscribed "%s"', self.id, topic)
+				self._log.info('BrkId: % 4s : Topic subscribed "%s"', self.id, topic)
 				return
 		except ValueError: # Only catch ValueError
 			pass
-		logging.error('BrkId: % 4s : Topic subscription failed "%s"', self.id, topic)
+		self._log.error('BrkId: % 4s : Topic subscription failed "%s"', self.id, topic)
 
 	def unsubscribe_topic(self, topic):
 		with self.mqttsub_lock:
 			if topic not in self.mqttsubscribedtopics:
-				logging.info('BrkId: % 4s : Can\'t unsubscribe not subscribed topic "%s"', self.id, topic)
+				self._log.info('BrkId: % 4s : Can\'t unsubscribe not subscribed topic "%s"', self.id, topic)
 				return
 			try:
 				res = self.mqttclient.unsubscribe(topic)
 				if res[0] == mqtt.MQTT_ERR_SUCCESS or res[0] == mqtt.MQTT_ERR_NO_CONN:
 					del self.mqttsubscribedtopics[topic]
-					logging.info('BrkId: % 4s : Topic unsubscribed "%s"', self.id, topic)
+					self._log.info('BrkId: % 4s : Topic unsubscribed "%s"', self.id, topic)
 					return
 			except ValueError: # Only catch ValueError
 				pass
-			logging.error('BrkId: % 4s : Topic unsubscription failed "%s"', self.id, topic)
+			self._log.error('BrkId: % 4s : Topic unsubscription failed "%s"', self.id, topic)
 
 	def publish(self, topic, payload, qos, retain):
 		if self.mqttclient is None:
-			logging.info('BrkId: % 4s : Could not send message Broker not started', self.id)
+			self._log.info('BrkId: % 4s : Could not send message Broker not started', self.id)
 			return
 		self.mqttclient.publish(topic, payload, qos, retain)
 		# Python Client : publish(topic, payload=None, qos=0, retain=False)
@@ -110,11 +111,11 @@ class jMqttClient:
 		#  - wait_for_publish() will block until the message is published. It will raise ValueError if the message is not queued (rc == MQTT_ERR_QUEUE_SIZE).
 		#  - is_published returns True if the message has been published. It will raise ValueError if the message is not queued (rc == MQTT_ERR_QUEUE_SIZE).
 		#  - A ValueError will be raised if topic is None, has zero length or is invalid (contains a wildcard), if qos is not one of 0, 1 or 2, or if the length of the payload is greater than 268435455 bytes.
-		logging.info('BrkId: % 4s : Sending message to broker (topic="%s", payload="%s", QoS=%s, retain=%s)', self.id, topic, payload, qos, retain)
+		self._log.info('BrkId: % 4s : Sending message to broker (topic="%s", payload="%s", QoS=%s, retain=%s)', self.id, topic, payload, qos, retain)
 
 	def start(self):
 		if self.mqttclient is not None:
-			logging.info('BrkId: % 4s : MqttClient.start() Already started', self.id)
+			self._log.info('BrkId: % 4s : MqttClient.start() Already started', self.id)
 			return
 		self.id = self.message['id']
 		self.mqtthostname = self.message['hostname']
@@ -129,13 +130,12 @@ class jMqttClient:
 		self.mqttsub_lock = threading.Lock()
 		self.mqttsubscribedtopics = {}
 		self.connected = False
-#		logging.debug('MqttClient.init() SELF dump: %r', [(attr, getattr(self, attr)) for attr in vars(self) if not callable(getattr(self, attr)) and not attr.startswith("__")])
+#		self._log.debug('MqttClient.init() SELF dump: %r', [(attr, getattr(self, attr)) for attr in vars(self) if not callable(getattr(self, attr)) and not attr.startswith("__")])
 
 		# Create MQTT Client
 		self.mqttclient = mqtt.Client(self.message['clientid'])
 		# Enable Paho logging functions
 		if 'paholog' in self.message and self.message['paholog'] != '':
-			logger = logging.getLogger()
 			self.mqttclient.enable_logger(logger)
 		else:
 			self.mqttclient.disable_logger()
@@ -149,7 +149,7 @@ class jMqttClient:
 				self.mqttclient.tls_set(ca_certs=self.message['tlscafile'], certfile=self.message['tlsclicertfile'], keyfile=self.message['tlsclikeyfile'])
 				self.mqttclient.tls_insecure_set(('tlsinsecure' in self.message) and self.message['tlsinsecure'])
 			except:
-				logging.exception('Fatal TLS Certificate import Exception, this connection will most likely fail!')
+				self._log.exception('Fatal TLS Certificate import Exception, this connection will most likely fail!')
 
 		self.mqttclient.reconnect_delay_set(5, 15)
 		self.mqttclient.on_connect = self.on_connect
@@ -159,8 +159,8 @@ class jMqttClient:
 			self.mqttclient.connect(self.mqtthostname, self.mqttport, 30)
 			self.mqttclient.loop_start()
 		except:
-			if logging.getLogger().isEnabledFor(logging.DEBUG):
-				logging.exception('BrkId: % 4s : MqttClient.start() Exception', self.id)
+			if self._log.getLogger().isEnabledFor(logging.DEBUG):
+				self._log.exception('BrkId: % 4s : MqttClient.start() Exception', self.id)
 
 	def stop(self):
 		if self.mqttclient is not None:
@@ -169,7 +169,7 @@ class jMqttClient:
 			self.mqttclient.disconnect()
 			self.mqttclient.loop_stop()
 			self.mqttclient = None
-		logging.debug('BrkId: % 4s : MqttClient.stop() ended', self.id)
+		self._log.debug('BrkId: % 4s : MqttClient.stop() ended', self.id)
 
 	def restart(self, message=None):
 		if message is not None:
