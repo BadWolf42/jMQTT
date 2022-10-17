2762c623b8580ac0eb70466cced0dd8ca11feb22
Bad <BadWolf42@users.noreply.github.com>
2022-10-17 14:38:31 +0200

    Remove deprecated js function 'jeedom.eqLogic.builSelectCmd'

--- desktop/js/jMQTT.js
+++ desktop/js/jMQTT.js
@@ -22,10 +22,6 @@ $('.eqLogicDisplayCard').on('click', 'span.hiddenAsCard', function(event) {
 	event.stopPropagation()
 });
 
-// Compatibility with Jeedom 4.1 // TODO Remove me when Jeedom 4.1 is deprecated
-if (typeof jeedom.eqLogic.buildSelectCmd !== 'function')
-	jeedom.eqLogic.buildSelectCmd = jeedom.eqLogic.builSelectCmd;
-
 //To memorise page refresh timeout when set
 var refreshTimeout;
 var timeout_refreshMqttClientInfo = null;
