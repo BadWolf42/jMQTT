BadWolf42/jMQTT

commit 85732a8e38dfbb4bfccab587ffcfc4834a2353ff

Rename brkId to eqLogic in eqLogic config and use Core eq removal

core/class/jMQTT.class.php
@@ -38,7 +38,7 @@ class jMQTT extends eqLogic {
 	const MQTTCLIENT_NOK                = 'nok';
 
 	const CONF_KEY_TYPE                 = 'type';
-	const CONF_KEY_BRK_ID               = 'brkId';
+	const CONF_KEY_BRK_ID               = 'eqLogic';
 	const CONF_KEY_MQTT_CLIENT_ID       = 'mqttId';
 	const CONF_KEY_MQTT_ADDRESS         = 'mqttAddress';
 	const CONF_KEY_MQTT_PORT            = 'mqttPort';
@@ -2355,7 +2355,7 @@ class jMQTT extends eqLogic {
 	 * @return jMQTT[]
 	 */
 	public static function byBrkId($id) {
-		$brkId = json_encode(array('brkId' => $id));
+		$brkId = json_encode(array('eqLogic' => $id));
 		/** @var jMQTT[] $eqpts */
 		$returns = self::byTypeAndSearchConfiguration(jMQTT::class, substr($brkId, 1, -1));
 		return $returns;

core/php/callback.php
@@ -88,7 +88,7 @@ foreach($messages as $message) {					// Iterate through the messages
 
 // TODO: Later
 /*
-		case 'value':								// {"cmd":"value", "c":[string], "v":string} #BrkId, cmdIds, value
+		case 'value':								// {"cmd":"value", "c":[string], "v":string} # c: list of cmdId v: value to set
 			if (!isset($message['c']) || !isset($message['v']))
 				break;
 			foreach($message['c'] as $cmdId)

desktop/js/jMQTT.js
@@ -322,7 +322,7 @@ $('.eqLogicAction[data-action=addJmqttBrk]').off('click').on('click', function (
 		if (result !== null) {
 			jeedom.eqLogic.save({
 				type: eqType,
-				eqLogics: [ $.extend({name: result}, {type: 'broker', brkId: -1}) ],
+				eqLogics: [ $.extend({name: result}, {type: 'broker', eqLogic: -1}) ],
 				error: function (error) {
 					$.fn.showAlert({message: error.message, level: 'danger'});
 				},
@@ -388,7 +388,7 @@ $('.eqLogicAction[data-action=addJmqttEq]').off('click').on('click', function ()
 			}
 			jeedom.eqLogic.save({
 				type: eqType,
-				eqLogics: [ $.extend({name: eqName}, {type: 'eqpt', brkId: broker}) ], // TODO Set brkId here
+				eqLogics: [ $.extend({name: eqName}, {type: 'eqpt', eqLogic: broker}) ],
 				error: function (error) {
 					$.fn.showAlert({message: error.message, level: 'danger'});
 				},
@@ -406,49 +406,6 @@ $('.eqLogicAction[data-action=addJmqttEq]').off('click').on('click', function ()
 $('.eqLogicAction[data-action=confEq]').off('click').on('click', function() {
 	var eqId = $(this).closest('div').attr('data-eqLogic_id');
 	$('#md_modal').dialog().load('index.php?v=d&modal=eqLogic.configure&eqLogic_id=' + eqId).dialog('open');
-});
-
-//
-// Remove / Move jMQTT equipment callback
-//
-$('.eqLogicAction[data-action=removeJmqtt]').off('click').on('click', function () {
-	function removeJmqtt() {
-		jeedom.eqLogic.remove({
-			type: eqType,
-			id: $('.eqLogicAttr[data-l1key=id]').value(),
-			error: function (error) {
-				$.fn.showAlert({message: error.message, level: 'danger'});
-			},
-			success: function () {
-				var url = jmqtt.initPluginUrl();
-				modifyWithoutSave = false;
-				url += '&removeSuccessFull=1';
-				loadPage(url);
-			}
-		});
-	}
-	if ($('.eqLogicAttr[data-l1key=id]').value() != undefined) {
-		var typB = $('.eqLogicAttr[data-l2key=type]').value() == 'broker';
-		bootbox.confirm('{{Etes-vous sûr de vouloir supprimer}}' + ' ' +
-				(typB ? '{{le broker}}' : "{{l'équipement}}") + ' <b>' + $('.eqLogicAttr[data-l1key=name]').value() + '</b> ?', function (result) {
-			if (result) {
-				if (typB) {
-					bootbox.confirm('<table><tr><td style="vertical-align:middle;font-size:2em;padding-right:10px"><span class="label label-warning"><i class="fas fa-exclamation-triangle" />' +
-						'</span></td><td style="vertical-align:middle">' + '{{Tous les équipements associés au broker vont être supprimés}}' +
-						'...<br><b>' + '{{Êtes vous sûr ?}}' + '</b></td></tr></table>', function (result) {
-						if (result) {
-							removeJmqtt();
-						}
-					});
-				}
-				else {
-					removeJmqtt();
-				}
-			}
-		});
-	} else {
-		$.fn.showAlert({message: '{{Veuillez d\'abord sélectionner un}} ' + eqType, level: 'danger'});
-	}
 });
 
 //
@@ -910,7 +867,7 @@ function printEqLogic(_eqLogic) {
 	}
 
 	// Show UI elements depending on the type
-	if ((_eqLogic.configuration.type == 'eqpt' && (_eqLogic.configuration.brkId == undefined || _eqLogic.configuration.brkId < 0))
+	if ((_eqLogic.configuration.type == 'eqpt' && (_eqLogic.configuration.eqLogic == undefined || _eqLogic.configuration.eqLogic < 0))
 			|| (_eqLogic.configuration.type != 'eqpt' && _eqLogic.configuration.type != 'broker')) { // Unknow EQ / orphan
 		$('.toDisable').addClass('disabled');
 		// $('.eqLogicAction[data-action="configure"]').removeClass('roundedLeft');
@@ -993,12 +950,12 @@ function printEqLogic(_eqLogic) {
 	}
 
 	// Initialize the broker dropbox
-	var brokers = $('.eqLogicAttr[data-l1key=configuration][data-l2key=brkId]');
+	var brokers = $('.eqLogicAttr[data-l1key=configuration][data-l2key=eqLogic]');
 	brokers.empty();
 	$.each( eqBrokers, function(key, name) {
 		brokers.append(new Option(name, key));
 	});
-	brokers.val(_eqLogic.configuration.brkId);
+	brokers.val(_eqLogic.configuration.eqLogic);
 }
 
 /**

desktop/php/jMQTT.php
@@ -208,7 +208,7 @@ function displayActionCard($action_name, $fa_icon, $attr = '', $class = '') {
 				<a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a>
 				<a class="btn btn-default btn-sm eqLogicAction typ-std toDisable" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a>
 				<a class="btn btn-success btn-sm eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
-				<a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="removeJmqtt"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>&nbsp;
+				<a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>&nbsp;
 			</div>
 			<div class="input-group pull-left" style="display:inline-flex">
 				<ul class="nav nav-tabs" role="tablist">

desktop/php/jMQTT_eqpt.php
@@ -50,7 +50,7 @@
 		<div class="form-group typ-std">
 			<label class="col-sm-3 control-label">{{Broker associé}}</label>
 			<div class="col-sm-3">
-				<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="brkId"></select>
+				<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="eqLogic"></select>
 			</div>
 		</div>
 

plugin_info/install.php
@@ -366,6 +366,20 @@ function moveCmdOutOfBrokers() {
 		$broker->save();
 	}
 }
+function modifyBrkIdConfKeyInEq() {
+	foreach (jMQTT::byType(jMQTT::class) as $eqLogic) {
+		if ($eqLogic->getType() == jMQTT::TYP_BRK) {
+			jMQTT::logger('debug', $eqLogic->getHumanName() . ' est un Broker');
+			continue;
+		}
+		// copy 'brkId' value to 'eqLogic' in broker config
+		$eqLogic->setConfiguration(jMQTT::CONF_KEY_BRK_ID, $eqLogic->getConfiguration('brkId', -1));
+		// delete 'brkId' from broker config
+		$eqLogic->setConfiguration('brkId', null);
+		$eqLogic->save(true); // Direct save to avoid issues while saving
+	}
+	jMQTT::logger('info', __("Clés de configuration des id des Brokers modifiées dans les équipements jMQTT", __FILE__));
+}
 
 function jMQTT_install() {
 	jMQTT::logger('debug', 'install.php: jMQTT_install()');
@@ -453,6 +467,7 @@ function jMQTT_update($_direct=true) {
 		// VERSION = 12
 		if ($versionFromDB < 12) {
 			moveCmdOutOfBrokers();
+			modifyBrkIdConfKeyInEq();
 			config::save(VERSION, 12, 'jMQTT');
 		}
 	}

commit 4be1d16e952dec52ac15c408f665438a10c5f5b6

Fix issue when adding new devices (introduced by 85732a8)

core/class/jMQTT.class.php
@@ -2341,6 +2341,10 @@ class jMQTT extends eqLogic {
 	public function setBrkId($id) {
 		$this->setConfiguration(self::CONF_KEY_BRK_ID, $id);
 	}
+	// Used by utils::a2o to set de Broker Id...
+	public function setEqLogic($id) {
+		$this->setConfiguration(self::CONF_KEY_BRK_ID, $id);
+	}
 
 	/**
 	 * Get this jMQTT object Qos