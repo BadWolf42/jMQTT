918080155849120162e926c306b2ccdeed117970
Bad <BadWolf42@users.noreply.github.com>
2023-11-25 19:16:24 +0100

    feat: add reviews button in plugin config page

--- desktop/js/jMQTT.config.js
+++ desktop/js/jMQTT.config.js
@@ -486,6 +486,9 @@ $(document).ready(function() {
     var dateVersion = $("#span_plugin_install_date").html();
     $("#span_plugin_install_date").empty().append("v" + version + " (" + dateVersion + ")");
 
+    // Add a link to the plugin rating
+    $('.bt_refreshPluginInfo').after('<a class="btn btn-success btn-sm" target="_blank" href="https://market.jeedom.com/index.php?v=d&p=market_display&id=3166"><i class="fas fa-comment-dots "></i> {{Avis}}</a>');
+
     if (!dStatus) {
         // Set Mosquitto status
         jmqtt_config.mosquittoStatus(mStatus);
