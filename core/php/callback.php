9f656e3c146c88f4d552ca59c437ecdec8c69770
Bad <BadWolf42@users.noreply.github.com>
2023-12-30 19:11:42 +0100

    remove last dirname(__FILE__)

--- core/php/callback.php
+++ core/php/callback.php
@@ -15,7 +15,7 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
+require_once __DIR__ . '/../../../../core/php/core.inc.php';
 
 
 class JmqttdCallbacks {
