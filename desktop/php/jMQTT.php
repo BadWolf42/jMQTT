9941d92c3a4e5d978c25e8d986c84e4b505dd147
Bad <BadWolf42@users.noreply.github.com>
2024-01-01 22:14:02 +0100

    put debug modal icon at the end and in red

--- desktop/php/jMQTT.php
+++ desktop/php/jMQTT.php
@@ -61,10 +61,10 @@ function displayActionCard($action_name, $fa_icon, $action = '', $class = 'logoS
                 displayActionCard('{{Configuration}}', 'fa-wrench', 'gotoPluginConf');
                 displayActionCard('{{Broker}}', 'fa-plus-circle', 'addJmqttBrk');
                 displayActionCard('{{Santé}}', 'fa-medkit', 'healthMQTT');
-                if (isset($_GET['debug']) || config::byKey('debugMode', 'jMQTT', "0") === "1")
-                    displayActionCard('Debug', 'fa-toolbox danger', 'debugJMQTT');
                 displayActionCard('{{Templates}}', 'fa-cubes', 'templatesMQTT');
                 displayActionCard('{{Équipement}}', 'fa-plus-circle', 'addJmqttEq', 'logoPrimary');
+                if (isset($_GET['debug']) || config::byKey('debugMode', 'jMQTT', "0") === "1")
+                    displayActionCard('Debug', 'fa-toolbox', 'debugJMQTT', 'danger');
                 ?>
                 </div>
             </div>
@@ -80,7 +80,7 @@ function displayActionCard($action_name, $fa_icon, $action = '', $class = 'logoS
                 <legend><i class="fas fa-comments"></i>&nbsp;Community</legend>
                 <div class="eqLogicThumbnailContainer">
                 <?php
-                 displayActionCard('{{Nouveau post}}', 'fa-ambulance', 'createCommunityPost');
+                displayActionCard('{{Nouveau post}}', 'fa-ambulance', 'createCommunityPost');
                 ?>
                 </div>
             </div>
