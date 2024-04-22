799d12680bef86e5fd178a09c5bdaa82a6f4f47c
Bad <BadWolf42@users.noreply.github.com>
2024-04-22 12:03:41 +0200

    feat: handle and send mqtt msg to Jeedom (old way)

--- resources/jmqttd_api/app/logics/cmd.py
+++ resources/jmqttd_api/app/logics/cmd.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 from aiomqtt import Message
+from binascii import b2a_base64, hexlify
 from logging import getLogger
 from typing import Dict
 from weakref import ref
+from zlib import decompress as zlib_decompress
 
+from callbacks import Callbacks
 from visitors.abstractvisitor import VisitableLogic, LogicVisitor
+from models.cmd import CmdInfoDecoderModel
 from models.unions import CmdModel
 
 
@@ -28,8 +32,92 @@ class CmdLogic(VisitableLogic):
             or '#' in self.model.configuration.topic
         )
 
+    async def _decompress(self, payload) -> str:
+        # jMQTT will try to decompress the payload (requested in issue #135)
+        try:
+            payload = zlib_decompress(payload, wbits=-15)
+            logger.debug(
+                'id=%i: decompressed payload: "0x%s"',
+                self.model.id,
+                (
+                    bytes(payload, 'utf-8')
+                    if isinstance(payload, str)
+                    else payload
+                ).hex()
+            )
+        except Exception:  # If payload cannot be decompressed
+            logger.debug(
+                'id=%i: could NOT decompress payload: "0x%s"',
+                self.model.id,
+                (
+                    bytes(payload, 'utf-8')
+                    if isinstance(payload, str)
+                    else payload
+                ).hex()
+            )
+        return payload
+
+    async def _decode(self, payload, decoder) -> str:
+        try:
+            payload = payload.decode('utf-8', decoder)
+            logger.debug(
+                'id=%i: decoded (%s) payload: "%s"',
+                self.model.id,
+                decoder.name,
+                payload
+            )
+        except Exception:
+            logger.info(
+                'id=%i: could NOT decode (%s) payload: "0x%s"',
+                self.model.id,
+                decoder.name,
+                (
+                    bytes(payload, 'utf-8')
+                    if isinstance(payload, str)
+                    else payload
+                ).hex()
+            )
+        return payload
+
+    async def _writeToFile(self, payload) -> str:
+        filename = f'file_{self.model.id}'
+        # TODO Callback to set file content = payload
+        logger.debug(
+            'id=%i: wrote to file "%s" payload: "0x%s"',
+            self.model.id,
+            filename,
+            (
+                bytes(payload, 'utf-8')
+                if isinstance(payload, str)
+                else payload
+            ).hex()
+        )
+        return f'{filename}?{ts}'
+
     async def handle(self, message: Message, ts: float):
-        pass
+        payload = message.payload
+        cfg = self.model.configuration
+        if cfg.tryUnzip:
+            payload = await self._decompress(payload)
+        if cfg.decoder != CmdInfoDecoderModel.none:
+            payload = await self._decode(payload, cfg.decoder)
+        if cfg.toFile:
+            payload = await self._writeToFile(payload)
+
+        logger.info(
+            'id=%i: payload="%s", QoS=%s, retain=%s',
+            self.model.id,
+            payload,
+            message.qos,
+            bool(message.retain)
+        )
+        await Callbacks.message(
+            self.weakBrk().model.id,
+            str(message.topic),
+            payload,
+            message.qos,
+            message.retain,
+        )
 
     # def getEqLogic(self):
     #     return self.weakEq()
