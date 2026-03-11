6fa61cd8b84ca2ed21721c6f5e06f914e97eb941
Bad <BadWolf42@users.noreply.github.com>
2026-03-11 22:12:57 +0100

    chore: Drop auto-discovery project

--- desktop/php/jMQTT.php
+++ desktop/php/jMQTT.php
@@ -207,10 +207,6 @@ function displayActionCard($action_name, $fa_icon, $action = '', $class = 'logoS
                     </tbody>
                 </table>
             </div>
-            <!--
-                TODO: Add here the "Discovery" tab
-                 labels: enhancement, html
-            -->
             <div role="tabpanel" class="tab-pane toDisable" id="commandtab">
                 <table id="table_cmd" class="table tree table-bordered table-condensed table-striped">
                     <thead>
